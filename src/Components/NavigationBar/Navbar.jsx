e98efd3adf7bde336be2f24f1cfa9b6b5e8a29c3
Yuwen-0 <halil.alagul0@gmail.com>
2023-12-24 13:36:53 +0300

    work pl

--- src/Components/NavigationBar/Navbar.jsx
+++ src/Components/NavigationBar/Navbar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import "./Navbar.css";
+import "/src/Components/NavigationBar/NavBar.css";
 export default function Navbar({isModalOpen,makeCircleBigger,makeCircleSmaller}) {
   const lastScrollTop = useRef(0);
 
